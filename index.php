fb56aacb83f2faea963d9d38904e3ec890742aa6
SAwarm <jonasdasilvaelias@gmail.com>
2022-01-06 21:41:13 -0300

    ADD: Trabalhando com os dados selecionados do banco - Aula 6, 7, 8

--- index.php
+++ index.php
@@ -37,13 +37,21 @@
         $query = 'SELECT * FROM tb_usuarios';
         
         $stmt = $con->query($query);
-        $list = $stmt->fetchAll();
 
-        echo '<pre>';
-        print_r($list);
-        echo '</pre>';
+        foreach($con->query($query) as $key => $value) {
+            print_r($value);
+            echo '<hr>';
+        }
+        //$list = $stmt->fetchAll(PDO::FETCH_ASSOC); // fetch, FETCH_OBJ
 
-        print_r($list[0]['nome']);
+        // echo '<pre>';
+        // print_r($list);
+        // echo '</pre>';
+
+        /*foreach ($list as $key => $value) {
+            print_r($value['nome']);
+            echo '<hr>';
+        }*/
 
     } catch (PDOException $ex) {
         echo 'Erro: ' . $ex->getCode() . ' Mensagem: ' . $ex->getMessage(); // MENSAGEM DE ERRO 
