48ddecb57e568b01796badf04416d3a690cb9bfe
SAwarm <jonasdasilvaelias@gmail.com>
2022-01-07 22:24:00 -0300

    ADD: Prevenção contra SQL INJECTIONS Aula 10

--- sql_injection.php
+++ sql_injection.php
@@ -8,14 +8,37 @@
         try {
             $con = new PDO($dsn, $user, $password); // CONECTION PDO
 
-            $query = "SELECT * FROM tb_usuarios where ";
+            /**
+             * NÃO SEGURO
+             */
+            /*$query = "SELECT * FROM tb_usuarios where ";
             $query .= " email= '{$_POST['usuario']}}' ";
             $query .= " AND senha = '{$_POST['senha']}'";
 
             $stmt = $conexao->query($query);
             $user = $stmt->fetch();
 
+            print_r($user);*/
+
+            /**
+             * MODO SEGURO
+             */
+            $query = "SELECT * FROM tb_usuarios where ";
+            $query .= " nome= :usuario ";
+            $query .= " AND senha = :senha ";
+
+            $stmt = $con->prepare($query);
+
+            $stmt->bindValue(':usuario', $_POST['usuario']); // bindValue utiliza uma variável reservado para preparar a query e ajudar contra SQL INJECTIONS
+            $stmt->bindValue(':senha', $_POST['senha']);
+
+            $stmt->execute();
+
+            $user = $stmt->fetch();
+
+            echo '<pre>';
             print_r($user);
+            echo '</pre>';
 
         } catch (Exception $ex) {
             echo 'Erro: ' . $ex->getCode() . ' Mensagem: ' . $ex->getMessage(); // MENSAGEM DE ERRO 
@@ -35,9 +58,9 @@
 </head>
 
 <body>
-    <form action="sql_injection.php">
-        <input type="text" placeholder="usuário">
-        <input type="password" placeholder="senha">
+    <form action="sql_injection.php" method="POST">
+        <input type="text" placeholder="usuário" name="usuario">
+        <input type="password" placeholder="senha" name="senha">
 
         <button type="submit">Enviar</button>
     </form>
